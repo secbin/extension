67d55bb0163470a0ff8d27f75d75f1ccdd4dccdc
Tyson Fairhurst <tyson.fairhurst@gmail.com>
2022-02-20 14:38:56 -0800

    Fetch request

--- src/hooks/usePasteBinPost.js
+++ src/hooks/usePasteBinPost.js
@@ -32,58 +32,74 @@ function usePasteBinPost(encryptQuery){
     const [ loading, setLoading ] = useState(false);
     const [ error, setError ] = useState(false);
   const apiKey = "wlhKnA7nnD5FDKtLebQLDUszOn6TituB";
-console.log(`https://pastebin.com/api/api_post.php/api_dev_key=${apiKey}&api_option=paste&api_paste_code=${encryptQuery}`);
-  useEffect(() => {
-   let ignore = false;
-   const controller = new AbortController();
-   async function fetchSearchResults() {
-    // let responseBody : string[] = [];
-    let responseBody = {};
-    //let responseBody = useState<string>('');
-     setLoading(true);
-     try {
-       const response = await fetch(
-        // `https://api.cl1p.net/${query}`,
-        // `https://cors-anywhere.herokuapp.com/https://api.cl1p.net/${query}`,
-          //`https://pastebin.com/raw/${query}`,
-            `https://cors-anywhere.herokuapp.com/https://pastebin.com/api/api_post.php/api_dev_key=${apiKey}&api_option=paste&api_paste_code=${encryptQuery}`,
-          //`https://cors-anywhere.herokuapp.com/https://scrape.pastebin.com/api_scrape_item.php?i=${query}`,
-          //`https://reqbin.com/echo/get/json`,
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded"},
-          signal: controller.signal,
+  // var request = new XMLHttpRequest();
+  //
+  // request.open("POST", "https://cors-anywhere.herokuapp.com/https://pastebin.com/api/api_post.php", true);
+  //
+  // request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  //
+  // request.send(`api_dev_key=${apiKey}&api_option=paste&api_paste_private=0&api_paste_code=${encryptQuery}`);
+  fetch("https://cors-anywhere.herokuapp.com/https://pastebin.com/api/api_post.php/", {
+  body: `api_dev_key=${apiKey}&api_option=paste&api_paste_private=0&api_paste_code=${encryptQuery}`,
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  method: "POST"
+}).then(res => console.log(res));
 
-         }
-       );
-       responseBody = await response.text();
-     } catch (e) {
-       if (e instanceof DOMException) {
-         console.log("== HTTP request cancelled")
-       } else {
-         setError(true);
-         throw e;
-       }
-     }
-     if (!ignore) {
-       setLoading(false);
-       setError(false);
-       console.log(responseBody);
-       setResult(responseBody || []);
-       //result = responseBody;
-     }
-   }
-   if (encryptQuery) {
-     fetchSearchResults()
-   }
-   return () => {
-     controller.abort();
-     ignore = true;
-   }
- }, [ encryptQuery ]);
-
-
- return [ result, error ];
+return [result, error];
+// console.log(`https://pastebin.com/api/api_post.php/api_dev_key=${apiKey}&api_option=paste&api_paste_code=${encryptQuery}`);
+//   useEffect(() => {
+//    let ignore = false;
+//    const controller = new AbortController();
+//    async function fetchSearchResults() {
+//     // let responseBody : string[] = [];
+//     let responseBody = {};
+//     //let responseBody = useState<string>('');
+//      setLoading(true);
+//      try {
+//        const response = await fetch(
+//         // `https://api.cl1p.net/${query}`,
+//         // `https://cors-anywhere.herokuapp.com/https://api.cl1p.net/${query}`,
+//           //`https://pastebin.com/raw/${query}`,
+//             `https://cors-anywhere.herokuapp.com/https://pastebin.com/api/api_post.php/api_dev_key=${apiKey}&api_option=paste&api_paste_code=${encryptQuery}`,
+//           //`https://cors-anywhere.herokuapp.com/https://scrape.pastebin.com/api_scrape_item.php?i=${query}`,
+//           //`https://reqbin.com/echo/get/json`,
+//           {
+//             method: "POST",
+//             headers: { "Content-Type": "application/x-www-form-urlencoded"},
+//           signal: controller.signal,
+//
+//          }
+//        );
+//        responseBody = await response.text();
+//      } catch (e) {
+//        if (e instanceof DOMException) {
+//          console.log("== HTTP request cancelled")
+//        } else {
+//          setError(true);
+//          throw e;
+//        }
+//      }
+//      if (!ignore) {
+//        setLoading(false);
+//        setError(false);
+//        console.log(responseBody);
+//        setResult(responseBody || []);
+//        //result = responseBody;
+//      }
+//    }
+//    if (encryptQuery) {
+//      fetchSearchResults()
+//    }
+//    return () => {
+//      controller.abort();
+//      ignore = true;
+//    }
+//  }, [ encryptQuery ]);
+//
+//
+//  return [ result, error ];
 
 
 
