00eb2a408c5ed660c2be74a90e44d504c2d526e4
Erik-Handeland <erikhandeland@gmail.com>
2022-02-20 16:28:03 -0800

    factored out encryption functions

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { ChromeMessage, Sender } from "../types";
-import { getCurrentTabUId, getCurrentTabUrl } from "../chrome/utils";
+import { getCurrentTabUId, getCurrentTabUrl, encryptText} from "../chrome/utils";
 import { Button, TextField } from "@mui/material";
-import forge from 'node-forge';
 
 export const Home = () => {
     const [url, setUrl] = useState<string>('');
@@ -52,33 +51,10 @@ export const Home = () => {
         });
     };
 
-    // https://www.cryptoexamples.com/javascript_forge_string_encryption_password_based_symmetric.html
-    const encryptText = () => {
-        // the password used for derviation of a key, assign your password here
-        // if none is assigned a random one is generated
-        let password = null;
-        if (password === null) {
-        password = forge.random.getBytesSync(48).toString();
-        }
+    const encryptWrapper = () => {
+        var result = encryptText(textbox, "password", "AES-GCM"); // password and Mode are optional
 
-        // derive key with password and salt
-        // keylength adheres to the "ECRYPT-CSA Recommendations" on "www.keylength.com"
-        let salt = forge.random.getBytesSync(128);
-        let key = forge.pkcs5.pbkdf2(password, salt, 10000, 32);
-
-        //create random initialization vector
-        let iv = forge.random.getBytesSync(16);
-
-        // ENCRYPT the text
-        let cipher = forge.cipher.createCipher("AES-GCM", key);
-        cipher.start({ iv: iv });
-        cipher.update(forge.util.createBuffer(textbox));
-        cipher.finish();
-        let tag = cipher.mode.tag;
-        let encrypted = forge.util.encode64(cipher.output.data);
-
-
-        setResponseFromContent(encrypted);
+        setResponseFromContent(result.CipherTXT);
     };
 
     var textbox = ""
@@ -97,7 +73,7 @@ export const Home = () => {
                     {url}
                 </p>
                 <TextField id="outlined-basic" label="Text" variant="outlined" onChange={textUpdate}/>
-                <Button variant="contained" onClick={encryptText}>Encrypt</Button>
+                <Button variant="contained" onClick={encryptWrapper}>Encrypt</Button>
                 <button onClick={sendTestMessage}>SEND MESSAGE</button>
                 <button onClick={sendRemoveMessage}>Remove logo</button>
                 <p>Response from content:</p>
