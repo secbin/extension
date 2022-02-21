5711ec20da4860211e1025b01a51f89de7fe49d6
Phillip Bindeman <bindeman@me.com>
2022-02-20 21:55:51 -0800

    add design

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,49 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { About } from "./routes/About";
 import { Home } from "./routes/Home";
+import { NewHome } from "./routes/NewHome";
+import SettingsIcon from '@mui/icons-material/Settings';
+import { Settings } from './routes/Settings'
+import { useHistory } from "react-router-dom";
+
+// import usePasteBinSearch from './hooks/usePasteBinSearch';
 import './App.css';
+import {AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import { ChevronLeft } from '@mui/icons-material';
 
 export const App = () => {
+    let {push, goBack} = useHistory();
     return (
+        <>
+        <div>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={() => { goBack()}}>
+                        <ChevronLeft />
+                    </IconButton>
+                    <Typography variant="h6" color="inherit" component="div">
+                        SecureBin
+                    </Typography>
+                    <IconButton edge="end" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={() => { push('/settings')}}>
+                        <SettingsIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </div>
         <Switch>
             <Route path="/about">
                 <About/>
+            </Route>
+            <Route path="/newhome">
+                <NewHome/>
+            </Route>
+            <Route path="/settings">
+                <Settings/>
             </Route>
             <Route path="/">
                 <Home/>
             </Route>
         </Switch>
+        </>
     )
 };
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,15 @@ import './index.css';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { StyledEngineProvider } from '@mui/material';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        <StyledEngineProvider injectFirst>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </StyledEngineProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
