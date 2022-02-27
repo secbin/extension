a8558cf04d4ffd3ee26719251ca75a9542f6aee8
fairhurt <55992872+fairhurt@users.noreply.github.com>
2022-02-27 15:43:16 -0800

    Fixing stuff

--- src/hooks/usePasteBinPost.js
+++ src/hooks/usePasteBinPost.js
@@ -6,30 +6,60 @@ function usePasteBinPost(encryptQuery){
     const [ loading, setLoading ] = useState(false);
     const [ error, setError ] = useState(false);
   const apiKey = "klVQoqGaWEZGAuv9qALhwCN94jyuBQ7w";
-if (encryptQuery){
+  const apiKey2 ="bYYcefkxnd18LJMggTKIH2Vg8m8QP-N0"
+// if (encryptQuery){
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
   myHeaders.append("Cookie", "_csrf-frontend=329554c223d6a49136d2267538fc128591f3ec2150a223caa1d6db2d96f0265aa%3A2%3A%7Bi%3A0%3Bs%3A14%3A%22_csrf-frontend%22%3Bi%3A1%3Bs%3A32%3A%22rO1MDUiUJzJoMpRxGyEtQ9KVFoodbesw%22%3B%7D; pastebin_posted=99663e9444444257d4931e06307949fe5a481efea6e1d02e1d14d0dd216f60dca%3A2%3A%7Bi%3A0%3Bs%3A15%3A%22pastebin_posted%22%3Bi%3A1%3Bs%3A8%3A%22JC4FD0vP%22%3B%7D");
-
+//
   var urlencoded = new URLSearchParams();
-  urlencoded.append("api_dev_key", "bYYcefkxnd18LJMggTKIH2Vg8m8QP-N0");
+  urlencoded.append("api_dev_key", apiKey);
   urlencoded.append("api_paste_code", encryptQuery);
   urlencoded.append("api_option", "paste");
 
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: urlencoded,
-    redirect: 'follow'
-  };
 
-  fetch("http://localhost:8080/https://pastebin.com/api/api_post.php", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-}
+useEffect(() => {
+ let ignore = false;
+ const controller = new AbortController();
+ async function fetchSearchResults() {
+  // let responseBody : string[] = [];
+  let responseBody = {};
+  //let responseBody = useState<string>('');
+   setLoading(true);
+   try {
+     const response = await fetch(
+       `https://cors.securebin.workers.dev/?https://pastebin.com/api/api_post.php`,
+        {
+        signal: controller.signal,
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+       }
+     );
+     responseBody = await response.text();
+   } catch (e) {
+     console.log(e);
+       setError(true);
+       throw e;
+     }
 
-
+   if (!ignore) {
+     setLoading(false);
+     setError(false);
+     console.log(responseBody);
+     setResult(responseBody || []);
+     //result = responseBody;
+   }
+ }
+ if (encryptQuery) {
+   fetchSearchResults()
+ }
+ return () => {
+   controller.abort();
+   ignore = true;
+ }
+}, [ encryptQuery ])
 
 return [result, error];
 
