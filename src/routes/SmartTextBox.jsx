b1032d70eaef95878ae4625bddb1dd465851d14a
Erik-Handeland <erikhandeland@gmail.com>
2022-03-15 12:04:45 -0700

    clean up

--- src/routes/SmartTextBox.jsx
+++ src/routes/SmartTextBox.jsx
@@ -1,12 +1,6 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import DirectionsIcon from '@mui/icons-material/Directions';
-import CustomizedMenus from '../routes/DropDownButton'
 
 export default function CustomizedInputBase() {
   return (
@@ -20,18 +14,6 @@ export default function CustomizedInputBase() {
             placeholder="Enter a PasteBin link or ciphertext to decrypt, plaintext to encrypt"
             inputProps={{ 'aria-label': 'search google maps', 'rows': '5'  }}
         />
-        {/*<IconButton type="submit" sx={{ p: '10px' }} aria-label="search">*/}
-        {/*  <SearchIcon />*/}
-        {/*</IconButton>*/}
-        {/*<Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />*/}
-        {/*<IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">*/}
-        {/*  <DirectionsIcon />*/}
-
-        {/*</IconButton>*/}
-        {/*  <Divider sx={{ height: 28, m: 0.5 }} orientation="horizontal" />*/}
-          {/*<CustomizedMenus/>*/}
-
-
       </Paper>
   );
 }
